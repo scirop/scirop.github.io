0e2b20ff5ab53df5278a3b0053e07c50e925f8a9
scirop <swarupsahoo@utexas.edu>
2019-09-05 11:49:06 -0500

    Updates

--- precache-manifest.366163c7fac80848f47c0868999ec0f4.js
+++ precache-manifest.366163c7fac80848f47c0868999ec0f4.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ed02d16caaf892c97f0da1d278eec7f3",
+    "revision": "68a215b9ca2339b62cbd3c7a6cdcf412",
     "url": "/index.html"
   },
   {
-    "revision": "7c657350c9f208e876c0",
+    "revision": "efffcf85734b39388f92",
     "url": "/static/css/main.5facb584.chunk.css"
   },
   {
-    "revision": "e73fc92228c75d450260",
-    "url": "/static/js/2.3b6ecf86.chunk.js"
+    "revision": "7a8d6910ff11daa90879",
+    "url": "/static/js/2.9f6e8308.chunk.js"
   },
   {
-    "revision": "7c657350c9f208e876c0",
-    "url": "/static/js/main.381f60cf.chunk.js"
+    "revision": "efffcf85734b39388f92",
+    "url": "/static/js/main.0760bc25.chunk.js"
   },
   {
     "revision": "b0f52d580a1186cd03aa",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a3d439c2ef7b8ff023185b9bea7384e.js"
+  "/precache-manifest.366163c7fac80848f47c0868999ec0f4.js"
 );
 
 self.addEventListener('message', (event) => {
